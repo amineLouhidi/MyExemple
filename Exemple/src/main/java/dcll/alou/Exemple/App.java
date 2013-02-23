3cd02fb1bcca57a20cbaaa6095d4e751b99ccae1
amine louhidi <a.louhidi@hotmail.fr>
2013-02-23 17:46:19 +0100

    un commit

---
+++ Exemple/src/main/java/dcll/alou/Exemple/App.java
@@ -0,0 +1,13 @@
+package dcll.alou.Exemple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
